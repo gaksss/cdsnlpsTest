a1683bd96a8a056e81a6522fc9bb8ae41c07d44f
gaksss <clement.carlier2@gmail.com>
2025-07-07 22:01:08 +0200

    correction

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 
 require_once('config.php');
 
-$stmt = $pdo->prepare("
-        SELECT * FROM catalogue
-        WHERE id = 120347855
-        LIMIT 50
-    ");
-$stmt->execute();
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// $stmt = $pdo->prepare("
+//         SELECT * FROM catalogue
+//         WHERE id = 120347855
+//         LIMIT 50
+//     ");
+// $stmt->execute();
+// $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 var_dump($results);
